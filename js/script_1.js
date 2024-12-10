20cdd86444fb92b52fd094d3ccee8a4ec5fa9288
Arsenii <supratuner@mail.ru>
2024-12-10 18:38:32 +0700

    Version 1.2.3
    Edit script files

--- js/script_1.js
+++ js/script_1.js
@@ -3,11 +3,14 @@ function exercise_1() { //Объявление функции
     let number_a = Number(document.getElementById("name_a_1").value); //Получение переменной по id
     let number_b = Number(document.getElementById("name_b_1").value); //Получение переменной по id
     let answer; //Создание переменной ответа
+    function get_middle_line_trap(a, b) {
+        return (a + b) / 2
+    }
     if ((isNaN(number_a) || (number_a == "")) || (isNaN(number_b) || (number_b == "")) || (number_a == number_b)) { //Объявление условия на проверку
         alert("Введите ещё раз корректные числа"); //Вывод о несоответствии ввода
         return;
     } else { //Если с первичной проверкой все хорошо, тогда
-        answer = (number_a + number_b) / 2; //Формула нахождения средней линии
+        answer = get_middle_line_trap(number_a, number_b); //Формула нахождения средней линии
         if (answer > 0) {
             document.getElementById("output_exercise_1").innerHTML = "Средняя линия вашей трапеции: " + answer + "."; //Вывод
             return;
